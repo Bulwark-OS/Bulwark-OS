e913c9091563668723c7657be571a5dda63718d8
Connor Jay <clad8895@proton.me>
2026-05-01 16:06:06 -0400

    test-installing kvmfr module via script

---
+++ scripts/installkvmfr.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+#################################
+# Kernel module
+#################################
+dnf install -y --setopt=install_weak_deps=False "kernel-devel-matched"
+
+dnf install -y --setopt=install_weak_deps=False akmods gcc-c++
+cp /usr/sbin/akmodsbuild /usr/sbin/akmodsbuild.backup
+# TODO remove this when fixed upstream
+sed -i '/if \[\[ -w \/var \]\] ; then/,/fi/d' /usr/sbin/akmodsbuild
+dnf install -y --setopt=install_weak_deps=False kvmfr-kmod
+mv /usr/sbin/akmodsbuild.backup /usr/sbin/akmodsbuild
+
+echo "Installing kmod..."
+akmods --force --kmod "kvmfr"
+
