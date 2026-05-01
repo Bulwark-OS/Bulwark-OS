bb6349486dfcb5e179543249f5f070ed38729204
Connor Jay <clad8895@proton.me>
2026-05-01 16:15:06 -0400

    Adding copr line for testing

--- files/scripts/installkvmfr.sh
+++ files/scripts/installkvmfr.sh
@@ -11,6 +11,7 @@ dnf install -y --setopt=install_weak_deps=False akmods gcc-c++
 cp /usr/sbin/akmodsbuild /usr/sbin/akmodsbuild.backup
 # TODO remove this when fixed upstream
 sed -i '/if \[\[ -w \/var \]\] ; then/,/fi/d' /usr/sbin/akmodsbuild
+dnf copr -y enable hikariknight/looking-glass-kvmfr
 dnf install -y --setopt=install_weak_deps=False kvmfr-kmod
 mv /usr/sbin/akmodsbuild.backup /usr/sbin/akmodsbuild
 
